54042954b9860f36a3c6a99f0da59f682c13e498
tonischweinstetter <tschweinstetter@gmail.com>
2025-04-30 17:40:18 +0200

    solved errors in git repository

---
+++ Quersumme/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Quersumme;
+
+class Program
+{
+    static int Quersumme(string zahl)
+    {
+        int quersumme = 0;
+        foreach (char c in zahl)
+        {
+            quersumme += (int) char.GetNumericValue(c);
+        }
+
+        return quersumme;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bitte gib eine Zahl ein, aus der die Quersumme gebildet werden soll:");
+
+        string zahl = Console.ReadLine();
+
+        Console.WriteLine($"Die Quersumme von {zahl} lautet {Quersumme(zahl)}!");
+    }
+}
